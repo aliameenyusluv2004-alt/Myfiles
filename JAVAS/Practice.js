414fdaef7a3f1d4af1900eadf3dc9bd9485f4d93
aliameenyusluv2004-alt <aliameenyusluv2004@gmail.com>
2026-04-07 15:25:33 +0100

    Add files via upload

---
+++ JAVAS/Practice.js
@@ -0,0 +1,416 @@
+
+// //JavaScript is a proggramming Language used to
+// console.log("hello world");
+// console.log("can you see the projector");
+
+// console.log("hi");
+
+// //variables
+// //variables is acontainer for storing values
+// let age = 19
+// console.log(age);
+// let name ="Joy"
+// console.log(name)
+// let animal ="Elephant"
+// animal = "Dog"
+// console.log(animal)
+
+// const month = "August"
+// //month = "july" //
+// console.log(month)
+
+
+// // Data types 
+// // string 
+// let food = "sweet potata"
+// let Drink = 'Fanta'
+// let place= `Alagomeji`
+
+// //Number
+// let salary = 16000;
+// //FLOAT
+// let cgpa =12.3
+// //boolean
+// const isPrimaryUser = true;
+// const isNewUser = false; //camelcase
+// const my_name_is = "joy" //snakecase
+
+
+// //undefined
+// let result;
+// console.log(result);
+
+// let day ;
+// console.log(day);
+// //Assignment Operator
+// //an Operator is a character that performs a task in our code.
+// // e.g assignment: =,  Add: +, Subtract: -, Divide: /, Remainder %
+
+// let y = 43
+// let x = 21
+// let z= y + x;
+// console.log(z)
+
+// let a = 56;
+// let b = 34;
+// let c = a-b;
+// console.log(c)
+// console.log(c*x)
+// console.log(z/a)
+// let j = z*a 
+// let s = b/x
+// console.log(j)
+// console.log(s)
+// //That remains after the right hand number divides into the left hand number evenly
+// //the remainder operator sometimes called MODULO, returns the number
+
+// let d =71
+// let v = 3
+// let p = 71%3
+// console.log(p)
+// console.log(11%3);
+// console.log(12%3)
+// let w = 30;
+// let u = (100+2) *w;
+// console.log(u); 
+// let g= 12
+// g+=5;
+// console.log(g);
+// let h = 30
+// h -= 5
+// console.log(h)
+// let r =87
+// r /= 3
+// console.log(r)
+// let i =92
+// i %= 6
+// console.log(i)
+
+// let o = 16
+// o *= 12
+
+// console.log(o)
+// let text1 = 20;
+// let text2 = 8;
+// let result2 = text1< text2;
+// console.log(result);
+
+// //STRING COCATENATION
+// // Is the process of appending one string to another.
+// console.log('one' + ',' + 'two' + ',' + 'three!i  ');
+// let exclamation = "what a very "
+// exclamation += "nice day"
+// console.log(exclamation);
+// console.log('my '+ 'name ' + 'is' +' King')
+// console.log('I' + ' am' + ' a' + ' Boy')
+
+        // examples of symbols
+// // let id = symbol(id);
+// let password = Symbol("ade654")
+//   let account = {
+//     usernameadmin: "Admin",
+//     [password]: "ade654",
+// }
+// for(let key in account){
+//     console.log(key);
+// }
+// console.log(Object.keys(account));
+// console.log(account[password]);
+
+    
+// let id = Symbol("id", "name", "age");
+// let User = {
+//     name : "Gideon",
+//     age : 19,
+//     id: 576544
+
+// }
+// console.log(User);
+// console.log(User.id);
+// console.log(User.age)
+// console.log(name);
+
+/// examples of objects
+let Drink ={
+    brand: 'coke',
+    weight : '2kg',
+    container : "bottle"
+};
+console.log(Drink);
+
+let human ={
+    name: "Esther",
+    age: "9",
+    height:"1feet",
+    complexion:"fair",
+    stature: "slim"
+}
+console.log(human)
+// Array
+let fruits = ["Apple", "Banana", "Mango"]
+console.log(fruits[2])
+console.log(fruits[1])
+console.log(fruits)
+// 
+let place=["yaba","Alajomeji","ijube", ]
+console.log(place)
+
+
+// add
+let name = ["seth", "Adams"]
+name.push("ibrahim");
+console.log(name)
+// Date data types 
+
+let current = new Date();
+console.log(current);
+
+let D1 = new Date("2025-08-28T12:56:42")
+console.log(D1)
+
+let D2 = new Date(2025,12,28,14,21,0)
+console.log(D2)
+let D3 = new Date(0)
+console.log(D3)
+let D4 = new Date()
+console.log(D4.getTime())
+// if/else Statement
+// this is a condition statement
+let Apple = "fruit";
+if (Apple = "fruit"){
+    console.log("it's a fruit" )
+};
+console.log()
+
+let Number = 10 ;
+if (Number = 10){
+    console.log("its an Integer")
+}
+let Ore = "smart "
+if (Ore>= "smart"){
+    console.log("She is serious")
+}
+else{
+    "Ore is not serious"
+}
+let age = 12
+if(age >= 18){
+    console.log("Eligible to vote ")
+}
+else{
+ console.log("you are not eligible  to vote")
+}
+let wale = 18000000000
+if (wale >= 1000000){
+    console.log("Alamin is a millionaire")
+} 
+else{
+    console.log("wale is poor")
+}
+let temperature =38;
+if (temperature<=35){
+    console.log("its too damn cold")
+}
+else{
+    console.log("the weather is hot")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
